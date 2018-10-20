0b3e9b76b4a0081a681bb44e5d64446b05c0d8cc
aurelien <aurelien.dejean@gmail.com>
2018-10-20 21:54:52 +0200

    FileInputStream and FileOutputStream

--- src/com/aurelien/formation/Test.java
+++ src/com/aurelien/formation/Test.java
@@ -1,20 +1,52 @@
 package com.aurelien.formation;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class Test {
     public static void main(String[] args) {
-        File f = new File("test.txt");
-        System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
-        System.out.println("Nom du fichier : " + f.getName());
-        System.out.println("Est-ce qu'il existe ? : " + f.exists());
-        System.out.println("Est-ce un répertoire ? : " + f.isDirectory());
-        System.out.println("Est-ce un fichier ? : " + f.isFile());
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
 
-        File f2 = new File("/");
-        for (File nom : f2.listFiles()) {
-            System.out.println(((nom.isDirectory()) ? nom.getName()+"/" : nom.getName()));
+        try {
+            fis = new FileInputStream(new File("test.txt"));
+            fos = new FileOutputStream(new File("test2.txt"));
+
+            byte[] buf = new byte[8];
+            int n = 0;
+
+            while ((n = fis.read(buf)) >= 0) {
+                fos.write(buf);
+
+                for (byte bit : buf) {
+                    System.out.print("\t" + bit + "(" + (char) bit + ")");
+                }
+                System.out.println("");
+                buf = new byte[8];
+            }
+            System.out.println("Copie terminée !");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
