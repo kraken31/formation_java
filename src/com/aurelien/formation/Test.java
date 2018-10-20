a9421c896061a34dd8d9db9d5d6f1a354424fb7b
aurelien <aurelien.dejean@gmail.com>
2018-10-20 17:43:58 +0200

    File

--- src/com/aurelien/formation/Test.java
+++ src/com/aurelien/formation/Test.java
@@ -1,10 +1,20 @@
 package com.aurelien.formation;
 
+import java.io.File;
+
 public class Test {
     public static void main(String[] args) {
-        Duo<String, Integer> dual = new Duo<String, Integer>("test", 2);
-        System.out.println("val1=" + dual.getValeur1() + " - val2=" + dual.getValeur2());
-        Duo<String, String> dual2 = new Duo<String, String>("test", "test");
-        System.out.println("val1=" + dual2.getValeur1() + " - val2=" + dual2.getValeur2());
+        File f = new File("test.txt");
+        System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
+        System.out.println("Nom du fichier : " + f.getName());
+        System.out.println("Est-ce qu'il existe ? : " + f.exists());
+        System.out.println("Est-ce un répertoire ? : " + f.isDirectory());
+        System.out.println("Est-ce un fichier ? : " + f.isFile());
+
+        File f2 = new File("/");
+        for (File nom : f2.listFiles()) {
+            System.out.println(((nom.isDirectory()) ? nom.getName()+"/" : nom.getName()));
+        }
+
     }
 }
